12e8e7a112c82f96d7541b6691a54b5690ee069b
Sophie <70588905+probablySophie@users.noreply.github.com>
2024-03-18 12:08:07 +0800

    Add files via upload

---
+++ disable_group_welcomes.ps1
@@ -0,0 +1,23 @@
+$M365Groups = Get-UnifiedGroup -ResultSize Unlimited  # Get every group available
+
+$count = 1;
+$len = $M365Groups.length;
+
+foreach ($group in $M365Groups)
+{
+	Write-Host -nonewline "$($count)/$($len) `t" # for prettiness
+
+	# if the current group has welcome messages enabled
+	if ($group.WelcomeMessageEnabled -eq $true)
+	{
+		# disable the welcome messages & send any output from that command into the bin
+		Set-UnifiedGroup $group.Identity -UnifiedGroupWelcomeMessageEnabled:$false *>$null
+		Write-Host -nonewline "updated     `t";
+	}
+	else
+	{
+		Write-Host -nonewline "already set `t";
+	}
+	Write-Host "$($group.DisplayName)";
+	$count++;
+}
