38e919de5abb5a0c479171bca54e38c892c65323
Sophie <>
2024-04-02 11:30:59 +0800

    Better output for the user :)

--- share_calendar_with_teammates.ps1
+++ share_calendar_with_teammates.ps1
@@ -10,9 +10,15 @@ $users = foreach ($user in $managed_users) {Get-MgUser -userid $user.id}
 # Give the permissions
 foreach ($user in $users)
 {
-	Write-Host "$($user.displayname): $($manager.displayname)"
+	Write-Host "$($user.displayname) <- $($manager.displayname)"
+	
 	# Give their manager write permissions
 	Add-MailboxFolderPermission -Identity "$($user.mail):\Calendar" -User "$($manager.mail)" -AccessRights Editor -SharingPermissionFlags Delegate
+
+	Write-Host "$($user.displayname) -> $($manager.displayname)"
+
+	# And vice-versa
+	Add-MailboxFolderPermission -Identity "$($manager.mail):\Calendar" -User "$($user.mail)" -AccessRights Reviewer
 
 	# Give read access to the teammates
 	foreach ($user2 in $users)
@@ -20,7 +26,7 @@ foreach ($user in $users)
 		# Don't give read access to themselves, that could probably break things
 		if ($user.mail -ne $user2.mail)
 		{
-			Write-Host "$($user.displayname): $($user2.displayname)"
+			Write-Host "$($user.displayname) <- $($user2.displayname)"
 			Add-MailboxFolderPermission -Identity "$($user.mail):\Calendar" -User "$($user2.mail)" -AccessRights Reviewer
 		}
 	}
